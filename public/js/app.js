1d464e48e6870e6c1414629baf6952633a40d0f8
William Lee <me@william-lee.com>
2016-03-30 16:42:32 +0100

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,10 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
+  var momentTimestamp = moment.utc(message.timestamp);
   console.log('New Message:');
   console.log(message.text);
-  $('.messages').append("<p>" + message.text + "</p>");
+  $('.messages').append("<p><strong>" + momentTimestamp.local().format('h:mm a') +  "</strong> " + message.text + "</p>");
 });
 
 // Handles submitting of new message
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,12 +15,17 @@ io.on('connection', function(socket) {
 
     //send to everyone BUT the sender io.emit sends to everyone.
     // socket.broadcast.emit('message', message);
+    message.timestamp = moment.valueOf();
     io.emit('message',message);
   });
 
+  //timestamp property - Javascript timestamp(milliseconds)
+
   socket.emit('message', {
-    text: 'Welcome to the chat application'
+    text: 'Welcome to the chat application',
+    timestamp: moment.valueOf()
   });
+
 });
 
 http.listen(PORT, function() {
